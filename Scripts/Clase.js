61a076f2add96f0360eb90da9ff0ef4ee95ad53f
zParraz <santiaguito.cps@gmail.com>
2025-09-05 23:19:00 -0500

    Añadido mensaje antes de mostrar la página

--- Scripts/Clase.js
+++ Scripts/Clase.js
@@ -1,19 +1,36 @@
-const edad = prompt("Cuál es tu edad?");
-if (edad >= 18 && edad <= 100 && edad >= 1 ) {
-    alert("Eres mayor de edad");
+// const edad = prompt("Cuál es tu edad?");
+// let usuario = prompt("¿Usuario?")
+// let contraseña = prompt("¿Contraseña?")
+
+alert("Bienvenido al homenaje de mi hermosa madre")
+document.body.classList.add("todo");
+
+function permiso() {
+    if (usuario === "Admin" && contraseña === "1234") {
+        document.body.classList.remove("todo"); // ✅ acceso permitido
+    } else if (!usuario && !contraseña){
+        document.body.classList.add("todo");    // ❌ acceso denegado
+    }
 }
-else if (edad <= 17 && edad >= 1) {
-    alert("Eres menor de edad")
-}
-else if (edad <= 0) {
-    alert("No has ni nacido hermano")
-}
-else if (edad >= 101){
-    alert("El limite es 100")
-}
-else {
-    alert("Ingresa valores permitidos")
-}
-function saludar(nombre) {
-    return "hola, " +  nombre
-}
+permiso();
+
+
+// if (edad >= 18 && edad <= 100 && edad >= 1 ) {
+// alert("Eres mayor de edad");
+// }
+// else if (edad <= 17 && edad >= 1) {
+//     alert("Eres menor de edad")
+// }
+// else if (edad <= 0) {
+//     alert("No has ni nacido hermano")
+// }
+// else if (edad >= 101){
+//     alert("El limite es 100")
+// }
+// else {
+//     alert("Ingresa valores permitidos")
+// }
+// function saludar(nombre) {
+//     return "hola, " +  nombre
+// }
+
